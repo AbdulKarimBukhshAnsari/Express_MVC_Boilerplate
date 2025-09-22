a7f1bd0ea127b744917d93a3969957e900e0b69b
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-09-22 23:59:15 +0500

    added

---
+++ src/app.js
@@ -0,0 +1,33 @@
+// this file is the entry point of the application
+
+import express from "express"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+
+const app = express();
+const CORS_ORIGIN = process.env.CORS_ORIGIN ;
+
+// defining the configurations
+
+// for defining the ORGIN URL by which the request could be made
+app.use(cors({
+    origin : CORS_ORIGIN ,
+    credentials : true 
+}))
+
+app.use(express.json({limit : '16kb'}));
+
+app.use(express.urlencoded({extended : true , limit : '16kb'}));
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
+
+// using routers
+import userRouter from "./routes/user.routes.js";
+
+app.use('/users' , userRouter) ;
+
+
+export default app ; 
